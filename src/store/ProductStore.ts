9ca53eef2cc0e16a469671c420c9e20b25ae8744
SukratUbha <54665232+SukratUbha@users.noreply.github.com>
2023-10-14 14:38:03 +1300

    product store added

---
+++ src/store/ProductStore.ts
@@ -0,0 +1,40 @@
+import { defineStore } from 'pinia'
+import axios from 'axios'
+
+type product_type = {
+  id: number
+  title: string
+  brand: string
+  category: string
+  description: string
+  discountPercentage: number
+  images: [string]
+  price: number
+  rating: number
+  stock: number
+  thumbnail: string
+}
+
+export const useProductStore = defineStore('product', {
+  state: () => ({
+    all_products: [] as product_type[],
+    discount_products: [] as product_type[]
+  }),
+
+  actions: {
+    async set_products() {
+      axios.get('https://dummyjson.com/products').then((res) => {
+        this.$state.all_products = res.data.products
+      })
+      console.log(this.$state.all_products)
+    },
+    async set_discount_products() {
+      axios.get('https://dummyjson.com/products').then((res) => {
+        const sortedProducts = res.data.products.sort(
+          (a:product_type, b:product_type) => b.discountPercentage - a.discountPercentage
+        )
+        this.$state.discount_products = sortedProducts.slice(0,8)
+      })
+    }
+  }
+})
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
   </main>
 </template>
+<!-- fetch('https://dummyjson.com/products')
+.then(res => res.json())
+.then(console.log);
+             -->
